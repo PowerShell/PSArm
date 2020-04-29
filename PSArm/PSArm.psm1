0e898af93be295eb9cd4b128f7c7896fdbe5f5a9
Robert Holt <rjmholt_msft@outlook.com>
2020-04-29 10:30:45 -0700

    Remove unrelated parts

--- PSArm/PSArm.psm1
+++
@@ -1,4 +0,0 @@
-$PSCmdlet.MyInvocation.MyCommand.ScriptBlock.Module.OnRemove = {
-    Set-Item Function:\TabExpansion2 (Get-Content -Raw Function:__OldTabExpansion2)
-    Remove-Item Function:__OldTabExpansion2
-}

40b02cf697b8c8790eb0489f2d0cc97bbcac842f
Robert Holt <rjmholt_msft@outlook.com>
2020-05-06 14:29:59 -0700

    Add type accelerators

---
+++ PSArm/PSArm/ArmTypeAccelerators.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Management.Automation;
+
+namespace PSArm
+{
+    public static class ArmTypeAccelerators
+    {
+        private static IReadOnlyDictionary<string, Type> s_armTypeAccelerators = new Dictionary<string, Type>
+        {
+            { "armvar", typeof(ArmVariable) }
+        };
+
+        private static Lazy<Dictionary<string, Type>> s_psTypeAcceleratorTable = new Lazy<Dictionary<string, Type>>(GetPSTypeAcceleratorsDict);
+
+        public static void Load()
+        {
+            foreach (KeyValuePair<string, Type> entry in s_armTypeAccelerators)
+            {
+                s_psTypeAcceleratorTable.Value[entry.Key] = entry.Value;
+            }
+        }
+
+        public static void Unload()
+        {
+            foreach (KeyValuePair<string, Type> entry in s_armTypeAccelerators)
+            {
+                s_psTypeAcceleratorTable.Value.Remove(entry.Key);
+            }
+        }
+
+        private static Dictionary<string, Type> GetPSTypeAcceleratorsDict()
+        {
+            return (Dictionary<string, Type>)typeof(PSObject)
+                .Assembly
+                .GetType("System.Management.Automation.TypeAccelerators")
+                .GetMethod("get_Get")
+                .Invoke(obj: null, Array.Empty<object>());
+        }
+    }
+}

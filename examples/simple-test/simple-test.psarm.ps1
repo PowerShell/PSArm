c1ab38116ab09d45fcbf8bde4cc6a1d7881f5882
Robert Holt <rjmholt_msft@outlook.com>
2021-03-26 16:13:32 -0700

    Add regression test

---
+++ examples/simple-test/simple-test.psarm.ps1
@@ -0,0 +1,52 @@
+
+param(
+    [Parameter(Mandatory)]
+    [string]
+    $storageAccountName,
+    
+    [Parameter(Mandatory)]
+    [string]
+    $location,
+
+    [Parameter()]
+    [string]
+    $accountType = 'Standard_LRS',
+
+    [Parameter()]
+    [string]
+    $kind = 'StorageV2',
+
+    [Parameter()]
+    [string]
+    $accessTier = 'Hot',
+    
+    [Parameter()]
+    [string]
+    $minimumTLSVersion = 'TLS1_2',
+    
+    [Parameter()]
+    [bool]
+    $supportsHTTPSTrafficOnly = 1,
+    
+    [Parameter()]
+    [bool]
+    $allowBlobPublicAccess = 1,
+    
+    [Parameter()]
+    [bool]
+    $allowSharedKeyAccess = 1
+)
+
+Arm {
+
+    Resource $storageAccountName -Provider 'Microsoft.Storage' -Type 'storageAccounts' -apiVersion '2019-06-01' -kind $kind -Location $location {
+        ArmSku $accountType
+        Properties {
+            accessTier $accessTier
+            minimumTLSVersion $minimumTLSVersion
+            supportsHTTPSTrafficOnly $supportsHTTPSTrafficOnly
+            allowBlobPublicAccess $allowBlobPublicAccess
+            allowSharedKeyAccess $allowSharedKeyAccess
+        }
+    }
+}

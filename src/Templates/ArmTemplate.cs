024bbcad86ae54d193050cc0a9616387c970dddd
Robert Holt <rjmholt_msft@outlook.com>
2021-03-26 16:18:52 -0700

    Fix NullReferenceException with parameters (#110)

--- src/Templates/ArmTemplate.cs
+++ src/Templates/ArmTemplate.cs
@@ -83,16 +83,19 @@ namespace PSArm.Templates
             var template = (ArmTemplate)InstantiateIntoCopy(new ArmTemplate(TemplateName), localParameters);
 
             // Sift through any parameters declared here and remove any we instantiated
-            ArmObject<ArmParameter> templateParameters = template.Parameters;
-            foreach (IArmString localParameter in localParameters.Keys)
+            if (template.Parameters is not null)
             {
-                templateParameters.Remove(localParameter);
-            }
+                ArmObject<ArmParameter> templateParameters = template.Parameters;
+                foreach (IArmString localParameter in localParameters.Keys)
+                {
+                    templateParameters.Remove(localParameter);
+                }
 
-            // If we instantiated all parameters, remove the parameter block
-            if (templateParameters.Count == 0)
-            {
-                template.Remove(ArmTemplateKeys.Parameters);
+                // If we instantiated all parameters, remove the parameter block
+                if (templateParameters.Count == 0)
+                {
+                    template.Remove(ArmTemplateKeys.Parameters);
+                }
             }
 
             return template;

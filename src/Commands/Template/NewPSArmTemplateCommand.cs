826821c1da33babb30f54c6babcc932c3c40a904
Robert Holt <rjmholt_msft@outlook.com>
2021-03-31 10:26:28 -0700

    Add alias scope restore and tests

--- src/Commands/Template/NewPSArmTemplateCommand.cs
+++ src/Commands/Template/NewPSArmTemplateCommand.cs
@@ -43,46 +43,51 @@ namespace PSArm.Commands.Template
                 }
             }
 
-            ScriptBlock transformedBody;
-            ArmObject<ArmParameter> armParameters;
-            ArmObject<ArmVariable> armVariables;
-            object[] psArgsArray;
-
-            using (var pwsh = PowerShell.Create(RunspaceMode.CurrentRunspace))
+            // Create the ARM template in an alias-free environment
+            ArmTemplate template = null;
+            using (PSAliasContext.EnterCleanAliasContext(SessionState))
             {
-                try
+                ScriptBlock transformedBody;
+                ArmObject<ArmParameter> armParameters;
+                ArmObject<ArmVariable> armVariables;
+                object[] psArgsArray;
+
+                using (var pwsh = PowerShell.Create(RunspaceMode.CurrentRunspace))
                 {
-                    transformedBody = new TemplateScriptBlockTransformer(pwsh).GetDeparameterizedTemplateScriptBlock(
-                        Body,
-                        out armParameters,
-                        out armVariables,
-                        out psArgsArray);
+                    try
+                    {
+                        transformedBody = new TemplateScriptBlockTransformer(pwsh).GetDeparameterizedTemplateScriptBlock(
+                            Body,
+                            out armParameters,
+                            out armVariables,
+                            out psArgsArray);
+                    }
+                    catch (Exception e)
+                    {
+                        this.ThrowTerminatingError(e, "TemplateScriptBlockTransformationFailure", ErrorCategory.InvalidArgument, Body);
+                        return;
+                    }
                 }
-                catch (Exception e)
+
+                template = new ArmTemplate(templateName);
+
+                if (armParameters is not null && armParameters.Count > 0)
                 {
-                    this.ThrowTerminatingError(e, "TemplateScriptBlockTransformationFailure", ErrorCategory.InvalidArgument, Body);
-                    return;
+                    template.Parameters = armParameters;
                 }
-            }
 
-            var template = new ArmTemplate(templateName);
-
-            if (armParameters is not null && armParameters.Count > 0)
-            {
-                template.Parameters = armParameters;
-            }
-
-            if (armVariables is not null && armVariables.Count > 0)
-            {
-                template.Variables = armVariables;
-            }
-
-            var templateBuilder = new ArmBuilder<ArmTemplate>(template);
-            foreach (PSObject output in InvokeCommand.InvokeScript(useLocalScope: true, transformedBody, input: null, psArgsArray))
-            {
-                if (output.BaseObject is ArmEntry armEntry)
+                if (armVariables is not null && armVariables.Count > 0)
                 {
-                    templateBuilder.AddEntry(armEntry);
+                    template.Variables = armVariables;
+                }
+
+                var templateBuilder = new ArmBuilder<ArmTemplate>(template);
+                foreach (PSObject output in InvokeCommand.InvokeScript(useLocalScope: true, transformedBody, input: null, psArgsArray))
+                {
+                    if (output.BaseObject is ArmEntry armEntry)
+                    {
+                        templateBuilder.AddEntry(armEntry);
+                    }
                 }
             }
 
---
+++ test/pester/assets/aliastest.psarm.ps1
@@ -0,0 +1,7 @@
+Arm {
+    Resource "mySubnet" -Namespace Microsoft.Network -ApiVersion 2019-11-01 -Type virtualNetworks/subnets {
+        properties {
+            addressPrefix 10.0.0.0/24
+        }
+    }
+}
